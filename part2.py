5f72df8d5650e618501a1c4daa5cf0925a8169c1
JohnEgan <jmegan1210@yahoo.com>
2022-02-10 17:50:49 -0500

    fixed extra code

--- part2.py
+++ part2.py
@@ -1,73 +1,12 @@
 # part 2 stuff
 
 class Word:
-    def init(self,word):
+    def _init_(self,word):
         self.value = word
 
 
 class Line:
-    def init(self):
-        self.words = []
-
-def main():
-    userInput = getInputFromFile()
-    shifted_output = circularShift(userInput)
-    print(shifted_output[0].words[0].value)
-    #sortedInput = bubbleSort(userInput)
-    #writeToFile(sortedInput)
-
-def getInputFromFile():
-    with open('input.txt', 'r') as file:
-        data = file.read().rstrip()
-    dataList = data.split(' ')
-
-    lineStorage = Line()
-    for i in dataList:
-        lineStorage.words.append(Word(i))
-    return lineStorage
-
-# first circular shift
-def circularShift(line):
-    new_sentences = []
-    current_line = line
-    for index in range(len(line.words)):
-        new_sentences.append(current_line)
-        new_line = Line()
-        new_line.words = current_line.words[1:] + [current_line.words[0]]
-    return new_sentences
-
-def bubbleSort(array):
-    length = len(array)
-    lowerSentences = []
-    for i in range(length):
-        lowerSentences.append(array[i].lower())
-
-    for x in range(length-1):
-        for y in range(length - x - 1):
-            if lowerSentences[y] > lowerSentences[y + 1]:
-                lowerSentences[y], lowerSentences[y + 1] = lowerSentences[y + 1], lowerSentences[y]
-
-def writeToFile(text):
-
-    string = ""
-    for i in range(len(text)):
-        for j in range(len(text[i])):
-            string += (text[i][j] + " ")
-        string += ("\n")
-    print(string)
-
-    with open('output.txt', 'w') as file:
-        file.write(string)
-
-main()# part 2 stuff
-
-class Word:
-    def init(self,word):
-        self.value = word
-
-
-class Line:
-    def init(self):
+    def _init_(self):
         self.words = []
 
 def main():
